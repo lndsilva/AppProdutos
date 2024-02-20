896a4b32d93907b80579efcc38c9ef53645f4389
lndsilva <lndsilva@hotmail.com>
2024-02-20 19:36:54 -0300

    criando janela de produtos

---
+++ app/src/main/java/br/com/etecia/appprodutos/ListaProdutosActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.appprodutos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaProdutosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lista_produtos_layout);
+    }
+}
