69682783395f59241b59190889a9563b6fd72aec
lndsilva <lndsilva@hotmail.com>
2024-02-20 22:16:30 -0300

    criando construtor

--- app/src/main/java/br/com/etecia/appprodutos/ProdutosAdapter.java
+++ app/src/main/java/br/com/etecia/appprodutos/ProdutosAdapter.java
@@ -1,12 +1,28 @@
 package br.com.etecia.appprodutos;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.card.MaterialCardView;
+
+import java.util.List;
+
 public class ProdutosAdapter extends RecyclerView.Adapter<ProdutosAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Produtos> listaProdutos;
+
+    //construtor da classe adapter
+    public ProdutosAdapter(Context context, List<Produtos> listaProdutos) {
+        this.context = context;
+        this.listaProdutos = listaProdutos;
+    }
 
     @NonNull
     @Override
@@ -25,12 +41,20 @@ public class ProdutosAdapter extends RecyclerView.Adapter<ProdutosAdapter.ViewHo
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        //declarando as variáveis que irão representar o xml no java
+        ImageView imgProduto;
+        TextView txtProduto;
+        MaterialCardView cardProduto;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            imgProduto = itemView.findViewById(R.id.idImgProduto);
+            txtProduto = itemView.findViewById(R.id.txtDescrProduto);
+            cardProduto = itemView.findViewById(R.id.idCardProduto);
+
+
         }
     }
-
 
 
 }
