9caf7bd888093a1adbbbb493cbd0c093d068ebc2
lndsilva <lndsilva@hotmail.com>
2024-02-27 20:11:57 -0300

    criando itens da lista de produtos

--- app/src/main/java/br/com/etecia/appprodutos/ListaProdutosActivity.java
+++ app/src/main/java/br/com/etecia/appprodutos/ListaProdutosActivity.java
@@ -1,14 +1,38 @@
 package br.com.etecia.appprodutos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListaProdutosActivity extends AppCompatActivity {
+RecyclerView idRecViewProdutos;
+List<Produtos> listaProdutos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lista_produtos_layout);
+
+        idRecViewProdutos = findViewById(R.id.idRecViewProdutos);
+
+        listaProdutos = new ArrayList<>();
+        //lista de produtos
+
+        ProdutosAdapter adapter = new ProdutosAdapter(getApplicationContext(),listaProdutos);
+
+        idRecViewProdutos.setLayoutManager(new GridLayoutManager(getApplicationContext(),2));
+
+        idRecViewProdutos.setAdapter(adapter);
+
+
+
+
+
     }
 }
--- app/src/main/java/br/com/etecia/appprodutos/ProdutosAdapter.java
+++ app/src/main/java/br/com/etecia/appprodutos/ProdutosAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -40,12 +41,22 @@ public class ProdutosAdapter extends RecyclerView.Adapter<ProdutosAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.txtProduto.setText(listaProdutos.get(position).getDescricao());
+        holder.imgProduto.setImageResource(listaProdutos.get(position).getImagem());
+
+        //clique no cardview do modelo
+        holder.cardProduto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"Teste",Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listaProdutos.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
